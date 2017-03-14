b13933a0e8634449a02638a46b11a40933f74c69
Pablo Gutierrez <pablo.gutierrezdiez@cognizant.com>
2017-03-14 20:54:06 +0100

    Remove data from Controller

--- src/app/questions.js
+++ src/app/questions.js
@@ -1,15 +1,6 @@
 module.exports = {
   template: require('./questions.html'),
   controller: function ($http, $log) {
-    this.callDone = false;
-    this.update = function (user) {
-      var data = {name: user.name, email: user.email, technical: user.technical};
-      $http.post('http://rtdapi.azurewebsites.net/api/users', data).then(function () {
-        $log.info('OK');
-        this.callDone = true;
-      }, function () {
-        $log.info('KO');
-      });
-    };
+
   }
 };
